8f395c6ffdda78870e8e80cc7a89fb05c457f07b
Daniel Nolan <DanNolan99@gmail.com>
2021-09-05 21:58:56 -0400

    add liquidity stage

--- projects/Liquidity Mining/0.7.5/Create Pool/setup/createPool.js
+++ projects/Liquidity Mining/0.7.5/Create Pool/setup/createPool.js
@@ -1,11 +1,18 @@
-const bigIntSqrt = require('./bigIntSqrt');
-
 async function createPool(poolInitializer, tokenAddress, wethAddress, tokenAmount, wethAmount) {
     
 }
 
+const bn = require("ganache-core/node_modules/bignumber.js");
+bn.config({ EXPONENTIAL_AT: 999999, DECIMAL_PLACES: 40 });
 function encodePriceSqrt(reserve1, reserve0) {
-    return bigIntSqrt(BigInt(reserve1) / BigInt(reserve0)) * (2n ** 96n);
+    return ethers.BigNumber.from(
+        new bn(reserve1.toString())
+            .div(reserve0.toString())
+            .sqrt()
+            .multipliedBy(new bn(2).pow(96))
+            .integerValue(3)
+            .toString()
+    )
 }
 
 module.exports = createPool;
