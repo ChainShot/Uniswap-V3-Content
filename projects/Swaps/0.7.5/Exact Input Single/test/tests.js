ccd70cb428ba00c863313bdecc88afe7b287fc92
Daniel Nolan <DanNolan99@gmail.com>
2021-09-22 23:26:24 -0400

    adds a swaps tutorial

---
+++ projects/Swaps/0.7.5/Exact Input Single/test/tests.js
@@ -0,0 +1,56 @@
+const { assert } = require("chai");
+
+const DAI_ADDR = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const WETH_ADDR = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+
+describe('Contract', function () {
+    let dai, weth, addr1, contract, deposit;
+    before(async () => {
+        [addr1] = await ethers.provider.listAccounts();
+        deposit = ethers.utils.parseEther("10000");
+        await modifyDaiBalance(addr1, deposit);
+
+        dai = await ethers.getContractAt("IERC20Minimal", DAI_ADDR);
+        weth = await ethers.getContractAt("IERC20Minimal", WETH_ADDR);
+
+        const Contract = await ethers.getContractFactory("Contract");
+        contract = await Contract.deploy();
+        await contract.deployed();
+
+        await dai.approve(contract.address, deposit);
+        await contract.addDai(deposit);
+    });
+
+    it("should hold the ether initially", async () => {
+        const balance = await dai.balanceOf(contract.address);
+        assert(balance.eq(deposit));
+    });
+
+    describe("trading DAI for WETH", () => {
+        before(async () => {
+            await contract.daiForWeth();
+        });
+
+        it("should hold no dai", async () => {
+            const balance = await dai.balanceOf(contract.address);
+            assert(balance.eq(0));
+        });
+
+        it("should hold weth", async () => {
+            const balance = await weth.balanceOf(contract.address);
+            assert(balance.gt(0));
+        });
+    });
+});
+
+async function modifyDaiBalance(addr, balance) {
+    const storageSlot = ethers.utils.hexZeroPad(2, "32");
+    const paddedAddr = ethers.utils.hexZeroPad(addr, "32");
+    const slot = ethers.utils.keccak256(paddedAddr + storageSlot.slice(2));
+
+    await network.provider.send("hardhat_setStorageAt", [
+        DAI_ADDR,
+        slot,
+        ethers.utils.hexZeroPad(balance, "32")
+    ]);
+}
