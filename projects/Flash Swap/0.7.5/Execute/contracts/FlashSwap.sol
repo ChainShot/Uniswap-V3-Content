e0c3886a5da74ad7b385935f651fc9e017b79129
Dan <DanNolan99@gmail.com>
2022-05-23 13:27:02 -0400

    use fee arg and cleanup

--- projects/Flash Swap/0.7.5/Execute/contracts/FlashSwap.sol
+++ projects/Flash Swap/0.7.5/Execute/contracts/FlashSwap.sol
@@ -4,34 +4,35 @@ pragma abicoder v2;
 
 import "@uniswap/v3-core/contracts/interfaces/IERC20Minimal.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-
-import "hardhat/console.sol";
 
 contract FlashSwap {
 	IUniswapV3Pool constant borrowPool = IUniswapV3Pool(0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168);
     ISwapRouter constant router = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 	IERC20Minimal constant weth = IERC20Minimal(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 	IERC20Minimal constant dai = IERC20Minimal(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+	uint constant borrowAmount = 100000e18;
 	
 	function execute() external {
-		borrowPool.flash(address(this), 100000e18, 0, "0xabcd");
+		borrowPool.flash(address(this), borrowAmount, 0, "0xabcd");
 	}
 
-	function uniswapV3FlashCallback(uint, uint, bytes calldata) external {
-		dai.approve(address(router), 100000e18);
+	function uniswapV3FlashCallback(uint fee0, uint, bytes calldata) external {
+		dai.approve(address(router), borrowAmount);
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams(
             address(dai),
             address(weth),
 			500,
 			address(this),
 			block.timestamp,
-            100000e18,
+            borrowAmount,
             0,
 			0
         );
         router.exactInputSingle(params);
+
+		uint repayAmount = borrowAmount + fee0;
 
 		weth.approve(address(router), weth.balanceOf(address(this)));
         ISwapRouter.ExactOutputSingleParams memory outputParams = ISwapRouter.ExactOutputSingleParams(
@@ -40,14 +41,12 @@ contract FlashSwap {
 			3000,
 			address(this),
 			block.timestamp,
-            100400e18,
+            repayAmount,
             weth.balanceOf(address(this)),
 			0
         );
         router.exactOutputSingle(outputParams);
 
-		dai.transfer(msg.sender, 100400e18);
-
-		console.log(weth.balanceOf(address(this)) / 1 ether);
+		dai.transfer(msg.sender, repayAmount);
 	}
 }
